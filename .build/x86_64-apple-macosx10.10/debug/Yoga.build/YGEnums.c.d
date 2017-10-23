6b4c8a0a1893c1c7cd5e50a3863c6297906a8d2e
Jake Marsh <jake@thejakemarsh.com>
2017-10-22 23:30:24 -0500

    Trying to get Swift PM support working.

---
+++ .build/x86_64-apple-macosx10.10/debug/Yoga.build/YGEnums.c.d
@@ -0,0 +1,4 @@
+dependencies: /Users/jakemarsh/github/yoga/yoga/YGEnums.c \
+  /Users/jakemarsh/github/yoga/yoga/include/module.modulemap \
+  /Users/jakemarsh/github/yoga/yoga/include/YGEnums.h \
+  /Users/jakemarsh/github/yoga/yoga/include/YGMacros.h
